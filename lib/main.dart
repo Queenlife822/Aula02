1c1af8c4fa320033fc266447ac7b820da393521d
Your Name~ <you@example.com>
2022-11-08 17:44:37 -0300

    login

--- lib/main.dart
+++ lib/main.dart
@@ -7,14 +7,12 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Aula02',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        
         primarySwatch: Colors.blueGrey,
       ),
       home: const MyHomePage(title: 'Smiliguido'),
@@ -25,8 +23,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
- 
-
   final String title;
 
   @override
@@ -34,66 +30,116 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
- 
+  set textAlign(TextAlign textAlign) {}
 
   @override
   Widget build(BuildContext context) {
-    
     return Scaffold(
-      
       body: Center(
-        child:Padding(
-          padding:const EdgeInsets.all(48.0) ,
-          child: Column (
-            mainAxisAlignment:MainAxisAlignment.center,
-            children : <Widget>[
-              const Icon(Icons.account_circle,             
-              size:200.50,
-              color:Colors.black,),
-              const SizedBox(
-                height:100 ,),
-              TextFormField ( 
-                decoration :const InputDecoration(
-                  prefixIcon:Icon(Icons.person),
-                  hintText: "Usuário",
-                ),
-              ), 
-              const SizedBox(
-                height: 40,
+        child: Padding(
+          padding: const EdgeInsets.all(48.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const Text(
+                "Bem-vindo",
+                style: TextStyle(
+                    color: Colors.blue,
+                    fontSize: 25,
+                    fontWeight: FontWeight.bold),
               ),
-                TextFormField(obscureText: true,
-                  decoration:const InputDecoration( 
-                    prefixIcon:Icon(Icons.key),
-                    suffixIcon:const Icon(Icons.visibility) ,
-                    hintText:"Senha",
+              const SizedBox(
+                height: 15,
+              ),
+              const Text(
+                "Faça login para continuar",
+              ),
+              const SizedBox(
+                height: 50,
+              ),
+              const Icon(
+                Icons.account_circle,
+                size: 100.50,
+                color: Colors.black,
+              ),
+              const SizedBox(
+                height: 50,
+              ),
+              TextFormField(
+                decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    prefixIcon: Icon(Icons.person),
+                    hintText: "Usuário",
+                    labelText: ("E-mail")),
+              ),
+              const SizedBox(
+                height: 25,
+              ),
+              TextFormField(
+                obscureText: true,
+                decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    prefixIcon: Icon(Icons.key),
+                    suffixIcon: const Icon(Icons.visibility),
+                    hintText: "Senha",
+                    labelText: ("Senha")),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: const [
+                  Text(
+                    "Esqueceu a senha?",
                   ),
-                  
-                ),const SizedBox(height: 80.0 ,
-                width:0.0 ,
-                ),
-               
-                ElevatedButton(
-                   // ignore: prefer_const_constructors
-                   style: ButtonStyle(
-                   backgroundColor: const MaterialStatePropertyAll<Color>(Colors.black),
-                   ),
-                onPressed:(){}, child:Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 250,vertical:30 ),
-                  child: Text("Login".toUpperCase(),style:const TextStyle(fontSize:25,
-                  fontWeight:FontWeight.bold)
+                ],
+              ),
+              const SizedBox(
+                height: 25,
+              ),
+              ElevatedButton(
+                  // ignore: prefer_const_constructors
+                  style: ButtonStyle(
+                    backgroundColor:
+                        const MaterialStatePropertyAll<Color>(Colors.black),
                   ),
-                )
+                  onPressed: () {},
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 90, vertical: 20),
+                    child: Text(
+                      "Login".toUpperCase(),
+                      style: const TextStyle(
+                          fontSize: 25, fontWeight: FontWeight.bold),
+                    ),
+                  )),
+              const SizedBox(
+                height: 10,
+              ),
+              RichText(
+                textAlign: TextAlign.center,
+                text: const TextSpan(
+                  style: TextStyle(color: Colors.black, fontSize: 36),
+                  children: <TextSpan>[
+                    TextSpan(text: 'Ainda não tem conta ?'),
+                    TextSpan(
+                      text: 'Clique aqui\n ',
+                      style: TextStyle(color: Colors.blueAccent),
+                    ),
+
+                    TextSpan(
+                      text: 'para criar conta ',
+                      style: TextStyle(color: Colors.blueAccent),
+                    )
+                  ],
                 ),
-              
+                textScaleFactor: 0.5,
+              )
             ],
           ),
         ),
-        ),
-         
-      
+      ),
     );
   }
 }
-
-
-
